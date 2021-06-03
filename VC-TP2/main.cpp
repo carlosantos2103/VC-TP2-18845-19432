a3a1a9e1e5386e77350904873aba773ce16b3228
Carlos Santos <carlosantos21.03@gmail.com>
2021-06-03 17:01:06 +0100

    update

--- VC-TP2/main.cpp
+++ VC-TP2/main.cpp
@@ -85,6 +85,7 @@ int main(void) {
 		vc_binary_blob_info(image[2], blobs, labels);
 
 		mainBlob.area = 0;
+		sinal = "";
 
 		if (blobs != NULL) {
 
@@ -105,7 +106,7 @@ int main(void) {
 			if (blobs[0].area > 7000) { //TODO: VERIFICAR SE E UM QUADRADO (OU PERTO)
 				mainBlob = blobs[0];
 				percent = mainBlob.perimeter * 100 / (float)mainBlob.area;
-				printf("mainBlob percent: %f\n", percent);
+				//printf("mainBlob percent: %f\n", percent);
 
 				if (percent >= 3.7 && percent <= 5.0) {	//setas
 					xCenter = mainBlob.x + mainBlob.width / 2;
@@ -124,7 +125,7 @@ int main(void) {
 							sinal = "Via de automoveis e motociclos";
 						}
 						else sinal = "Auto-Estrada";
-						printf("%f\t%f\t%f\t\n", percent2, percent3, percent4);
+						//printf("%f\t%f\t%f\t\n", percent2, percent3, percent4);
 					}
 					else
 						sinal = "Auto-Estrada";
@@ -132,79 +133,62 @@ int main(void) {
 				}
 			}
 		}
-		printf("\n\t\t%d", mainBlob.area);
+		
 		if (mainBlob.area == 0) {
 			free(blobs);
-			printf("REd");
-			vc_hsv_red_segmentation(image[1], image[2], 345, 7, 38, 70, 85, 100); // H S V VERMELHO H200-230 S79-93 V48-60 
-			vc_binary_open(image[2], image[3], 3, 5);
-			/*blobs = vc_binary_blob_labelling(image[3], image[2], &labels);*/
 
-			//if (blobs != NULL) {
+			vc_hsv_red_segmentation(image[1], image[2], 350, 9, 35, 75, 80, 100); // H S V VERMELHO H200-230 S79-93 V48-60 
+			vc_binary_open(image[2], image[3], 3, 7);
+			blobs = vc_binary_blob_labelling(image[3], image[2], &labels);
+			vc_binary_blob_info(image[2], blobs, labels);
 
-			//	// Ordenação dos blobs por ordem decrescente tendo em conta a sua área
-			//	for (i = 0; i < labels - 1; i++)
-			//	{
-			//		for (j = i + 1; j < labels; j++)
-			//		{
-			//			if (blobs[i].area < blobs[j].area)
-			//			{
-			//				auxBlob = blobs[i];
-			//				blobs[i] = blobs[j];
-			//				blobs[j] = auxBlob;
-			//			}
-			//		}
-			//	}
+			if (blobs != NULL) {
 
-			//	if (blobs[0].area > 7000) { //TODO: VERIFICAR SE E UM QUADRADO (OU PERTO)
-			//		mainBlob = blobs[0];
-			//		percent = mainBlob.perimeter * 100 / (float)mainBlob.area;
-			//		printf("RED - mainBlob percent: %f\n", percent);
+				// Ordenação dos blobs por ordem decrescente tendo em conta a sua área
+				for (i = 0; i < labels - 1; i++)
+				{
+					for (j = i + 1; j < labels; j++)
+					{
+						if (blobs[i].area < blobs[j].area)
+						{
+							auxBlob = blobs[i];
+							blobs[i] = blobs[j];
+							blobs[j] = auxBlob;
+						}
+					}
+				}
+				if (blobs[0].area > 7000) { //TODO: VERIFICAR SE E UM QUADRADO (OU PERTO)
+					mainBlob = blobs[0];
+					percent = mainBlob.perimeter * 100 / (float)mainBlob.area;
+					//printf("RED - mainBlob percent: %f\n", percent);
 
-			//		//if (percent >= 3.7 && percent <= 5.0) {	//setas
-			//		//	xCenter = mainBlob.x + mainBlob.width / 2;
-			//		//	//printf("%d - %d\n", xCenter, mainBlob.xc);
-			//		//	if (mainBlob.xc > xCenter)
-			//		//		sinal = "Obrigatorio virar a esquerda";
-			//		//	else if (mainBlob.xc < xCenter)
-			//		//		sinal = "Obrigatorio virar a direita";
-			//		//}
-			//		//else if (percent > 5.1) { // AUTO ESTRADA OU CARRO
-			//		//	if (labels > 3) {
-			//		//		percent2 = blobs[1].area * 100 / (float)mainBlob.area;
-			//		//		percent3 = blobs[2].area * 100 / (float)mainBlob.area;
-			//		//		percent4 = blobs[3].area * 100 / (float)mainBlob.area;
-			//		//		if (percent2 >= 13.0 && percent2 < 17.0 && percent3 >= 0.8 && percent3 < 2.7 && percent4 >= 0.8 && percent4 < 2.7) {
-			//		//			sinal = "Via de automoveis e motociclos";
-			//		//		}
-			//		//		else sinal = "Auto-Estrada";
-			//		//		printf("%f\t%f\t%f\t\n", percent2, percent3, percent4);
-			//		//	}
-			//		//	else
-			//		//		sinal = "Auto-Estrada";
-
-			//		//}
-			//	}
-			//}
+					if (percent >= 6.8 && percent <= 10.0) {	//STOP
+						sinal = "STOP";
+					}
+					else if (percent > 3.5) { // PROIBIDO
+						sinal = "PROIBIDO";
+					}
+				}
+			}
 		}
 
 
-		//free(blobs);
-		/*if (mainBlob.area != 0) {
+		free(blobs);
+		if (sinal != "") {
 			blobs = (OVC*)calloc((1), sizeof(OVC));
 			blobs[0] = mainBlob;
 			vc_draw_bouding_box(image[2], image[0], blobs, 1);
 			vc_draw_center_mass(image[2], image[0], blobs, 1);
 			free(blobs);
-		}*/
+		}
 
 
 		vc_gray_to_rgb(image[3], image[1]);
 
 
 		// Copia dados de imagem da estrutura IVC para uma estrutura cv::Mat
-		memcpy(frame.data, image[0]->data, video.width* video.height * 3);
-		if (mainBlob.area != 0) {
+		memcpy(frame.data, frame2.data, video.width* video.height * 3);
+		if (sinal != "") {
 			str = std::string("SINAL: ").append(sinal);
 			cv::putText(frame, str, cv::Point(20, 25), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 0, 0), 2);
 			cv::putText(frame, str, cv::Point(20, 25), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 255), 1);
